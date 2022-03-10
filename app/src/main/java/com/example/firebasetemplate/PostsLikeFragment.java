97261450a1418dfd215f28e974358fdcfd7ae576
andreupasalamar <andreupas23@gmail.com>
2022-03-10 19:43:07 +0100

    M06 Likes arreglat

--- app/src/main/java/com/example/firebasetemplate/PostsLikeFragment.java
+++ app/src/main/java/com/example/firebasetemplate/PostsLikeFragment.java
@@ -5,6 +5,6 @@ import com.google.firebase.firestore.Query;
 public class PostsLikeFragment extends PostsHomeFragment{
     @Override
     Query setQuery() {
-        return db.collection("posts").whereEqualTo("likes." + auth.getUid(), true);
+        return db.collection("posts").whereEqualTo("favs." + auth.getUid(), true);
     }
 }
